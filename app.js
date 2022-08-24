7c341d175d5feb322e6fa40f8f041a04246a0509
FilstonMbambi <mbambifilston038@gmail.com>
2022-08-24 23:03:33 +0200

    added some features

--- app.js
+++ app.js
@@ -2,4 +2,14 @@
 const container = document.querySelector(".container");
 let blocks = document.querySelectorAll(".blocks");
 
-console.log(blocks)
+console.log(blocks);
+// Players
+const player = (playerChoice) => {
+    const choices = document.querySelectorAll("#playerChoice");
+    choices.forEach(choice => choice.addEventListener("click", () => {
+        playerChoice = choice.textContent;
+    }));
+    return {playerChoice}
+}
+// Add event listeners on blocks
+blocks.forEach(block => block.addEventListener("click", () => {block.textContent = `${"X"}`}))
