6da286e14fa96bd33e1e2288da5a5ff418495592
FilstonMbambi <mbambifilston038@gmail.com>
2022-08-29 15:04:32 +0200

    added some features

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
 // DOM Elements
 const container = document.querySelector(".container");
 let blocks = document.querySelectorAll(".blocks");
+const restartBtn = document.getElementById("restart");
 
 console.log(blocks);
-// Players
+
+// Player
 const player = (playerChoice) => {
-    const choices = document.querySelectorAll("#playerChoice");
+
+    const choiceContainer = document.querySelector("#playerChoice");
+    const choices = document.querySelectorAll("#choice");
     choices.forEach(choice => choice.addEventListener("click", () => {
-        playerChoice = choice.textContent;
+        if (choice.textContent === "X") {
+            playerChoice = "X";
+        } else {
+            playerChoice = "O";
+        }
+        choiceContainer.classList.add("invisible");
+        restartBtn.classList.remove("invisible");
+        // Add event listeners on blocks
+        blocks.forEach(block => block.addEventListener("click", () => {block.textContent = `${playerChoice}`}));
     }));
-    return {playerChoice}
+    return playerChoice
 }
-// Add event listeners on blocks
-blocks.forEach(block => block.addEventListener("click", () => {block.textContent = `${"X"}`}))
+player()
+
+// Computer
+const computer = (board, computerChoice) => {
+
+}
