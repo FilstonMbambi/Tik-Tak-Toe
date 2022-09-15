9849f67ff74484efe4e82e30dd672e98302d93b7
FilstonMbambi <mbambifilston038@gmail.com>
2022-09-15 23:38:16 +0200

    modified computer function

--- app.js
+++ app.js
@@ -11,12 +11,12 @@ restartBtn.addEventListener("click", () => {
         block.textContent = ""
         // block.removeEventListener("click");
     });
-    player().choiceContainer.classList.remove("invisible");
+    player.choiceContainer.classList.remove("invisible");
     restartBtn.classList.add("invisible");
 })
 
 // Player
-const player = () => {
+const player = (() => {
 
     let _playerChoice
     const choiceContainer = document.querySelector("#playerChoice");
@@ -30,27 +30,36 @@ const player = () => {
         choiceContainer.classList.add("invisible");
         restartBtn.classList.remove("invisible");
         // Add event listeners on blocks
-        blocks.forEach(block => block.addEventListener("click", () => {block.textContent = `${_playerChoice}`}));
+        blocks.forEach(block => {
+            block.addEventListener("click", () => {block.textContent = `${_playerChoice}`})
+            gameBoards.setField(block.getAttribute("id"), _playerChoice);
+        });
     }));
     return {_playerChoice, choiceContainer}
-}
-player()
+})()
+
 // Board
-const gameBoard = ((board, item) => {
-    board = [0,0,0,
-             0,0,0,
-             0,0,0];
-    blocks.forEach(block => block.setAttribute("id", board.indexOf(item)));
+const gameBoards = ((board, item) => {
+    board = ["","","",
+             "","","",
+             "","",""];
+    const setField = (index, sign) => {
+        board[index] = sign;
+    }
+    return {setField};
 })()
 
 // Computer
-const computer = (board, computerChoice) => {
-    // 0 = empty, 1 = X, 2 = O
-   
-    let playerChoice = player()._playerChoice;
+const computer = () => {
+    let computerChoice;
+    let playerChoice = player._playerChoice;
+
     playerChoice === "X"? computerChoice = "O":computerChoice = "X";
-    for (let i = 0; i < board.length; i++) {
+    blocks.forEach(block => {
         let randIndex = Math.floor((Math.random) * 8);
-        board[randIndex][randIndex] = computerChoice;
-    }
+        gameBoards.setField(randIndex, computerChoice);
+        block.textContent = computerChoice;
+    })
+
+    return {computerChoice};
 }
