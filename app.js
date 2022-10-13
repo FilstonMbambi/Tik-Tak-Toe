2c40e64683aef7eca4cb5a6d5e075acfa6b45bfe
FilstonMbambi <mbambifilston038@gmail.com>
2022-10-13 19:42:58 +0200

    fixed restart button

--- app.js
+++ app.js
@@ -4,23 +4,26 @@ let blocks = document.querySelectorAll(".blocks");
 const restartBtn = document.getElementById("restart");
 
 
-restartBtn.addEventListener("click", () => {
-    blocks.forEach(block => {
-        block.textContent = "";
-        gameBoards.setField(block.getAttribute("id"), "");
-        // block.removeEventListener("click");
-    });
-    player.choiceContainer.classList.remove("invisible");
-    restartBtn.classList.add("invisible");
-})
-
 // Player
 const player = (() => {
-
+    
     let _computerChoice;
-    let _playerChoice
+    let _playerChoice;
     const choiceContainer = document.querySelector("#playerChoice");
     const choices = document.querySelectorAll("#choice");
+    
+    // Add event listeners on blocks
+    const events = field => {
+        field.textContent = `${_playerChoice}`;
+        gameBoards.setField(field.getAttribute("id"), _playerChoice);
+
+        let randIndex = Math.floor((Math.random()) * 8);
+        gameBoards.setField(randIndex, _computerChoice);
+        let randField = document.getElementById(`${randIndex}`);
+        randField.textContent = _computerChoice;
+        // console.log(gameBoards.board);
+    }
+    
     choices.forEach(choice => choice.addEventListener("click", () => {
         if (choice.textContent === "X") {
             _playerChoice = "X";
@@ -32,22 +35,16 @@ const player = (() => {
         choiceContainer.classList.add("invisible");
         restartBtn.classList.remove("invisible");
 
-        // Add event listeners on blocks
-        blocks.forEach(block => {
-            block.addEventListener("click", () => {
-                block.textContent = `${_playerChoice}`;
-                gameBoards.setField(block.getAttribute("id"), _playerChoice);
 
-                let randIndex = Math.floor((Math.random()) * 8);
-                gameBoards.setField(randIndex, _computerChoice);
-                let field = document.getElementById(`${randIndex}`);
-                field.textContent = _computerChoice;
-                console.log(gameBoards.board);
+        blocks.forEach(block => {
+            block.addEventListener("click", (e) => {
+                if (block.textContent !== "") return;
+                events(block)
             });
         });
-
+        
     }));
-    return { _playerChoice, choiceContainer, _computerChoice }
+    return { _playerChoice, choiceContainer, _computerChoice, events}
 })()
 
 // Board
@@ -56,11 +53,16 @@ const gameBoards = (() => {
                  "", "", "",
                  "", "", ""];
     const setField = (index, sign) => {
-        if (board[index] = sign) return;
         board[index] = sign;
     }
     return { setField, board };
 })()
+
+restartBtn.addEventListener("click", () => {
+    window.location.reload();
+    player.choiceContainer.classList.remove("invisible");
+    restartBtn.classList.add("invisible");
+})
 
 // Computer
 const computer = (block) => {
